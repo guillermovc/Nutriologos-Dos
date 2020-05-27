39533ae7b1c68b08f7dba35399f8c1133cbff9f7
AndrewRiv <43888898+AndrewRiv@users.noreply.github.com>
2020-05-27 13:11:02 -0700

    Add files via upload

--- NutriologosDos/src/ventanas/PanelAgregarCita.java
+++ NutriologosDos/src/ventanas/PanelAgregarCita.java
@@ -9,12 +9,12 @@ import clases.Conexion;
 import javax.swing.JTextField;
 import java.awt.Color;
 import java.sql.Connection;
-import java.sql.Date;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import javax.swing.JOptionPane;
 import java.text.SimpleDateFormat;
+import java.util.Date;
 /**
  *
  * @author Andrew
@@ -41,11 +41,10 @@ public class PanelAgregarCita extends javax.swing.JPanel {
         jLabel2 = new javax.swing.JLabel();
         txt_pacienteID = new javax.swing.JTextField();
         jLabel3 = new javax.swing.JLabel();
-        txt_fecha = new javax.swing.JTextField();
         jLabel4 = new javax.swing.JLabel();
         txt_descripcion = new javax.swing.JTextField();
         btnAgregarCita = new javax.swing.JButton();
-        jLabel5 = new javax.swing.JLabel();
+        jdcFecha = new com.toedter.calendar.JDateChooser();
 
         jLabel1.setText("Agregar Cita");
 
@@ -62,8 +61,7 @@ public class PanelAgregarCita extends javax.swing.JPanel {
             }
         });
 
-        jLabel5.setForeground(new java.awt.Color(153, 153, 153));
-        jLabel5.setText("ejemplo: 2020-05-24");
+        jdcFecha.setDateFormatString("dd/MM/yyyy");
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(this);
         this.setLayout(layout);
@@ -75,19 +73,12 @@ public class PanelAgregarCita extends javax.swing.JPanel {
                     .addComponent(jLabel4)
                     .addComponent(jLabel3)
                     .addComponent(jLabel2))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addGroup(layout.createSequentialGroup()
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
-                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                            .addComponent(txt_pacienteID, javax.swing.GroupLayout.PREFERRED_SIZE, 100, javax.swing.GroupLayout.PREFERRED_SIZE)
-                            .addComponent(txt_descripcion, javax.swing.GroupLayout.PREFERRED_SIZE, 165, javax.swing.GroupLayout.PREFERRED_SIZE))
-                        .addGap(0, 40, Short.MAX_VALUE))
-                    .addGroup(layout.createSequentialGroup()
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                            .addComponent(jLabel5)
-                            .addComponent(txt_fecha, javax.swing.GroupLayout.PREFERRED_SIZE, 114, javax.swing.GroupLayout.PREFERRED_SIZE))
-                        .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))))
+                    .addComponent(txt_pacienteID, javax.swing.GroupLayout.PREFERRED_SIZE, 100, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(txt_descripcion, javax.swing.GroupLayout.PREFERRED_SIZE, 165, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(jdcFecha, javax.swing.GroupLayout.PREFERRED_SIZE, 146, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addGap(0, 46, Short.MAX_VALUE))
             .addGroup(layout.createSequentialGroup()
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addGroup(layout.createSequentialGroup()
@@ -107,13 +98,11 @@ public class PanelAgregarCita extends javax.swing.JPanel {
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(txt_pacienteID, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                     .addComponent(jLabel2))
-                .addGap(26, 26, 26)
-                .addComponent(jLabel5)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(txt_fecha, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(jLabel3))
-                .addGap(34, 34, 34)
+                .addGap(43, 43, 43)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                    .addComponent(jLabel3)
+                    .addComponent(jdcFecha, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addGap(37, 37, 37)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(txt_descripcion, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                     .addComponent(jLabel4))
@@ -124,10 +113,13 @@ public class PanelAgregarCita extends javax.swing.JPanel {
     }// </editor-fold>//GEN-END:initComponents
 
     private void btnAgregarCitaActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnAgregarCitaActionPerformed
-        String descripcion, fecha, pacienteID;
+        String descripcion, pacienteID;
+        Date date = jdcFecha.getDate();
         int invalidos = 0;
-
-        JTextField[] campos = {txt_descripcion,txt_fecha,txt_pacienteID};
+        long d = date.getTime();
+        
+        JTextField[] campos = {txt_descripcion,txt_pacienteID};
+        java.sql.Date fecha = new java.sql.Date(d);
 
         // Recorremos los arreglos
         for (JTextField campo : campos) {
@@ -146,7 +138,6 @@ public class PanelAgregarCita extends javax.swing.JPanel {
         else {
             // Guardamos todo lo de los campos de texto en nuestras variables
             descripcion = txt_descripcion.getText().trim();
-            fecha = txt_fecha.getText().trim();
             pacienteID = txt_pacienteID.getText().trim();
 
             // Ya que obtuvimos los datos nos preparamos para hacer un query en la base de datos
@@ -169,7 +160,7 @@ public class PanelAgregarCita extends javax.swing.JPanel {
                 if(rs.next()){
                     pst.setInt(3,Integer.parseInt(rs.getString("id")));
                 }
-                pst.setDate(4, Date.valueOf(fecha));
+                pst.setDate(4, fecha);
                 if(RS.next()){
                     pst.setString(5, RS.getString("fecha_registro"));
                 } else {
@@ -214,9 +205,8 @@ public class PanelAgregarCita extends javax.swing.JPanel {
     private javax.swing.JLabel jLabel2;
     private javax.swing.JLabel jLabel3;
     private javax.swing.JLabel jLabel4;
-    private javax.swing.JLabel jLabel5;
+    private com.toedter.calendar.JDateChooser jdcFecha;
     private javax.swing.JTextField txt_descripcion;
-    private javax.swing.JTextField txt_fecha;
     private javax.swing.JTextField txt_pacienteID;
     // End of variables declaration//GEN-END:variables
 }
